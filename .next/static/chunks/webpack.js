f49def3c98c16454dc0e28a5de28685f54092679
Renatha Victor <renatha.nvictor@gmail.com>
2020-12-15 17:30:05 -0300

    configure to prod

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "04b42d9bf917001f7adc";
+/******/ 	var hotCurrentHash = "29d135a680a708289d08";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- utils/api.js
+++ utils/api.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 // require('dotenv').config()
 
-// const {
-//   REACT_APP_PROJECT_ENDPOINT,
-//   REACT_APP_API_ENV
-// } = process.env
+const {
+  REACT_APP_PROJECT_ENDPOINT,
+  REACT_APP_API_ENV
+} = process.env
 
 const urls = {
   'development': 'http://localhost:3000',
-  'production': 'http://localhost:3000'
+  'production': REACT_APP_PROJECT_ENDPOINT
 }
 
 // const currentHost = backendHosts[REACT_APP_API_ENV] || 'http://localhost:3000'
